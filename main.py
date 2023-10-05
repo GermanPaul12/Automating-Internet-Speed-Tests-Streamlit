de3ca41eadbf1f32a8a13c3e15ac996a920cf64d
GermanPaul12 <german.paul@icloud.com>
2023-10-05 23:35:43 +0200

    update code

--- main.py
+++ main.py
@@ -1,51 +1,43 @@
+import os
+import re
+import subprocess
+import time
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-import os
-import re
-import datetime as dt
-import matplotlib.pyplot as plt
-# pip install st-gsheets-connection
-#os.system("speedtest-cli > speed.txt")
+# Guide https://pimylifeup.com/raspberry-pi-internet-speed-monitor/
 
-os.system("speedtest-cli --secure > speed.txt")
-new_row = []
-now = dt.datetime.now()
-# Create a connection object.
+response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+
+ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
+download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
+upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
+jitter = re.search('Latency:.*?jitter:\s+(.*?)ms', response, re.MULTILINE)
+
+ping = ping.group(1)
+download = download.group(1)
+upload = upload.group(1)
+jitter = jitter.group(1)
+
+try:
+    f = open('/home/german/Documents/Coding/Automating-Internet-Speed-Tests-Streamlit/backup.csv', 'a+')
+    if os.stat('/home/german/Documents/Coding/Automating-Internet-Speed-Tests-Streamlit/backup.csv').st_size == 0:
+            f.write('Date,Time,Ping (ms),Jitter (ms),Download (Mbps),Upload (Mbps)\r\n')
+except:
+    pass
+
+f.write('{},{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, jitter, download, upload))
+
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 
 df = conn.read(
     ttl="10m",  # Time to cache the data
-    usecols=[0, 1, 2, 3],)  # Which columns to read
+    usecols=[0, 1, 2, 3, 4, 5],)  # Which columns to read
     #nrows=3) # How many rows to read
 # returns DataFrame
-pattern = r"(\d+\.\d+) Mbit/s"
-pattern_ping = r"(\d+\.\d+) ms"
+df.loc[len(df)] = [time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, jitter, download, upload]
 
-with open("speed.txt", "r") as f:
-    data = f.readlines() 
-
-if len(data) >= 9:
-    ping = re.search(pattern_ping, data[4])
-    download = re.search(pattern, data[6])
-    upload = re.search(pattern, data[8])
-    if ping:
-        print("Ping: ", ping.groups()[0])
-        new_row.append(float(ping.groups()[0]))
-    if download:
-        print("Download: ", download.groups()[0])
-        new_row.append(float(download.groups()[0]))
-    if upload:
-        print("Upload: ", upload.groups()[0])
-        new_row.append(float(upload.groups()[0]))
-new_row.append(now.strftime(r"%d/%m/%Y %H:%M:%S"))
-
-if len(new_row) == 4: df.loc[len(df)] = new_row
-else: 
-    print("Error", new_row)
-
-df.to_csv("backup.csv", index=False)
 
 conn.update(data=df) # Update the data with a given DataFrame.
 # Print results.
